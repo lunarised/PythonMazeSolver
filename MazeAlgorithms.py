a60c23cb3b8de2b910a70938a71e079b8d8486db
lunarised <lunarised@outlook.com>
2020-04-05 12:36:17 +1200

    Added some tests

--- MazeAlgorithms.py
+++ MazeAlgorithms.py
@@ -4,7 +4,7 @@ class DepthFirstSolve:
     image = None
     def __init__(self, image):
         self.image = image
-        for row in range(self.image.size[0]):
-            for col in range(self.image.size[1]):
-                print(row)
-                print (self.image.getpixel((col, row)))
+        for col in range(self.image.size[0]):
+            for row in range(self.image.size[1]):
+                
+                print (self.image.getpixel((row, col)))
